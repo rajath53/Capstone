1f8fa10b9bedb2d6b2581f2f8ca93ac2fe56b680
rajath53 <74061153+rajath53@users.noreply.github.com>
2023-04-03 15:33:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,74 +1,94 @@
+import numpy as np
+import matplotlib.pyplot as plt
 import pickle
-import streamlit as st
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import AdaBoostClassifier
+
+
+from sklearn.preprocessing import OrdinalEncoder
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
 import pandas as pd
-from PIL import Image
+import streamlit as st
+oe = OrdinalEncoder()
 
-model = pickle.load( open("model.pkl",'rb'))
+#[theme]
+backgroundColor="#2b10d6"
+secondaryBackgroundColor="#2975ce"
+textColor="#fafafb"
 
-def main():
-	add_selectbox = st.sidebar.selectbox(
-	"How would you like to predict?",
-	("Online", "Batch"))
-	st.sidebar.info('This app is created to predict Customer Churn')
-	st.title("Predicting Customer Churn")
-	if add_selectbox == 'Online':
-		gender = st.selectbox('Gender:', ['male', 'female'])
-		seniorcitizen= st.selectbox(' Customer is a senior citizen:', [0, 1])
-		partner= st.selectbox(' Customer has a partner:', ['yes', 'no'])
-		dependents = st.selectbox(' Customer has  dependents:', ['yes', 'no'])
-		phoneservice = st.selectbox(' Customer has phoneservice:', ['yes', 'no'])
-		multiplelines = st.selectbox(' Customer has multiplelines:', ['yes', 'no', 'no_phone_service'])
-		internetservice= st.selectbox(' Customer has internetservice:', ['dsl', 'no', 'fiber_optic'])
-		onlinesecurity= st.selectbox(' Customer has onlinesecurity:', ['yes', 'no', 'no_internet_service'])
-		onlinebackup = st.selectbox(' Customer has onlinebackup:', ['yes', 'no', 'no_internet_service'])
-		deviceprotection = st.selectbox(' Customer has deviceprotection:', ['yes', 'no', 'no_internet_service'])
-		techsupport = st.selectbox(' Customer has techsupport:', ['yes', 'no', 'no_internet_service'])
-		streamingtv = st.selectbox(' Customer has streamingtv:', ['yes', 'no', 'no_internet_service'])
-		streamingmovies = st.selectbox(' Customer has streamingmovies:', ['yes', 'no', 'no_internet_service'])
-		contract= st.selectbox(' Customer has a contract:', ['month-to-month', 'one_year', 'two_year'])
-		paperlessbilling = st.selectbox(' Customer has a paperlessbilling:', ['yes', 'no'])
-		paymentmethod= st.selectbox('Payment Option:', ['bank_transfer_(automatic)', 'credit_card_(automatic)', 'electronic_check' ,'mailed_check'])
-		tenure = st.number_input('Number of months the customer has been with the current telco provider :', min_value=0, max_value=240, value=0)
-		monthlycharges= st.number_input('Monthly charges :', min_value=0, max_value=240, value=0)
-		totalcharges = tenure*monthlycharges
-		output= ""
-		output_prob = ""
-		input_dict={
-				"gender":gender ,
-				"seniorcitizen": seniorcitizen,
-				"partner": partner,
-				"dependents": dependents,
-				"phoneservice": phoneservice,
-				"multiplelines": multiplelines,
-				"internetservice": internetservice,
-				"onlinesecurity": onlinesecurity,
-				"onlinebackup": onlinebackup,
-				"deviceprotection": deviceprotection,
-				"techsupport": techsupport,
-				"streamingtv": streamingtv,
-				"streamingmovies": streamingmovies,
-				"contract": contract,
-				"paperlessbilling": paperlessbilling,
-				"paymentmethod": paymentmethod,
-				"tenure": tenure,
-				"monthlycharges": monthlycharges,
-				"totalcharges": totalcharges
-			}
 
-		if st.button("Predict"):
-			y_pred = model
-			churn = y_pred = 0
-			output_prob = float(y_pred)
-			output = bool(churn)
-		st.success('Churn: {0}, Risk Score: {1}'.format(output, output_prob))
-	if add_selectbox == 'Batch':
-		file_upload = st.file_uploader("Upload csv file for predictions", type=["csv"])
-		if file_upload is not None:
-			data = pd.read_csv(file_upload)
-			y_pred = model
-			churn = y_pred = 1.0
-			churn = bool(churn)
-			st.write(churn)
+model = open('model.pkl','rb')
+classifier = pickle.load(model)
 
-if __name__ == '__main__':
-	main()
+#scale = open('scaler.pkl','rb')
+#scaler = pickle.load(scale)
+
+st.title("Telco_Customer_Churn")
+st.header("Predicting Churned or not")
+
+df = pd.read_csv("data.csv")
+df = df.head()
+
+nav = st.sidebar.radio("Navigation", ["Home","Prediction","About us"])
+if nav == "Home":
+    if st.checkbox("Show table"):
+        st.table(df)
+
+    val = st.slider("Filter df using MonthlyCharges",0,100)
+    df=df.loc[df["MonthlyCharges"]>=val]
+    
+    graph = st.selectbox("What kind of graph ?", ["MonthlyCharges"])
+    if graph == "MonthlyCharges":
+        fig, ax = plt.subplots()
+        ax.scatter(df['MonthlyCharges'],df['TotalCharges'])
+        plt.title('Scatter')
+        plt.ylim(0)
+        plt.xlabel("MonthlyCharges")
+        plt.ylabel("TotalCharges")
+        st.pyplot(fig)
+
+if nav == "Prediction":
+    st.header("Know your Churn")
+    gender = st.selectbox('Gender:', ['male', 'female'])
+    SeniorCitizen= st.selectbox(' Customer is a senior citizen:', [0, 1])
+    Partner= st.selectbox(' Customer has a partner:', ['yes', 'no'])
+    Dependents = st.selectbox(' Customer has  dependents:', ['yes', 'no'])
+    PhoneService = st.selectbox(' Customer has phoneservice:', ['yes', 'no'])
+    MultipleLines = st.selectbox(' Customer has multiplelines:', ['yes', 'no', 'no_phone_service'])
+    InternetService= st.selectbox(' Customer has internetservice:', ['dsl', 'no', 'fiber_optic'])
+    OnlineSecurity= st.selectbox(' Customer has onlinesecurity:', ['yes', 'no', 'no_internet_service'])
+    OnlineBackup = st.selectbox(' Customer has onlinebackup:', ['yes', 'no', 'no_internet_service'])
+    DeviceProtection = st.selectbox(' Customer has deviceprotection:', ['yes', 'no', 'no_internet_service'])
+    TechSupport = st.selectbox(' Customer has techsupport:', ['yes', 'no', 'no_internet_service'])
+    StreamingTV = st.selectbox(' Customer has streamingtv:', ['yes', 'no', 'no_internet_service'])
+    StreamingMovies = st.selectbox(' Customer has streamingmovies:', ['yes', 'no', 'no_internet_service'])
+    Contract= st.selectbox(' Customer has a contract:', ['month-to-month', 'one_year', 'two_year'])
+    PaperlessBilling = st.selectbox(' Customer has a paperlessbilling:', ['yes', 'no'])
+    PaymentMethod= st.selectbox('Payment Option:', ['bank_transfer_(automatic)', 'credit_card_(automatic)', 'electronic_check' ,'mailed_check'])
+    tenure = st.number_input('Number of months the customer has been with the current telco provider :', min_value=0, max_value=240, value=0)
+    MonthlyCharges= st.number_input('Monthly charges :', min_value=0, max_value=240, value=0)
+    TotalCharges = tenure*MonthlyCharges
+
+    val = [[gender, SeniorCitizen, Partner, Dependents, tenure, PhoneService, MultipleLines, InternetService, OnlineSecurity, OnlineBackup, DeviceProtection, TechSupport, StreamingTV, StreamingMovies, Contract, PaperlessBilling, PaymentMethod, MonthlyCharges, TotalCharges]]
+    val = np.array(val)
+    X_test = pd.DataFrame(val)
+
+    # Encoding the categorical data
+    df_cat = X_test.select_dtypes("object")
+    for i in df_cat:
+        X_test[i]=oe.fit_transform(X_test[[i]])
+
+    # Scaling the dataset
+    scaler = StandardScaler()
+    X_test = scaler.fit_transform(X_test)
+
+
+    pred = classifier.predict(X_test)[0]
+    if st.button("Predict"):
+        if pred == 1:
+            st.success("Your prediction is: Yes")
+        elif pred == 0:
+            st.success("Your prediction is: No")
+        else:
+            st.error("Invalid prediction value")
